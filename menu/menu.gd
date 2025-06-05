7706095985fb3d1c5f3225b3bbb4ff24bfafcb12
Chicken502 <bcnelson100@gmail.com>
2025-06-04 20:23:18 -0400

    add transition to menu scene

--- menu/menu.gd
+++ menu/menu.gd
@@ -4,7 +4,7 @@ enum difficult {EASY, MEDIUM, HARD}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$TransitionLayer/AnimationPlayer.play("Fade")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
